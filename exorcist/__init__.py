0e1831d53c4ad62247299bce4256a00983fbbe3e
David W.H. Swenson <david.swenson@omsf.io>
2023-04-19 18:24:50 -0500

    Add __init__, updates to models

---
+++ exorcist/__init__.py
@@ -0,0 +1,2 @@
+from .taskdb import TaskStatusDB, NoStatusChange
+from .models import TaskStatus, Task
--- exorcist/models.py
+++ exorcist/models.py
@@ -1,23 +1,29 @@
 from enum import Enum
-from dataclasses import dataclass
-from datetime import datetime
+from typing import NamedTuple, TypeVar, Generic
+
+# generics: the actual types here depend on the client library
+Result = TypeVar("Result")
+TaskDetails = TypeVar("TaskDetails")
 
 class TaskStatus(Enum):
+    """
+    Status of a given task.
+    """
     BLOCKED = 0
     AVAILABLE = 1
     IN_PROGRESS = 2
     RESULTS_READY = 3
     COMPLETED = 99
+    TOO_MANY_RETRIES = -2
     ERROR = -1
 
 
-@dataclass
-class Task:
-    taskid: str
-    taskfile: str
-    last_modified: datetime
-    n_retries: int
+# TODO: it isn't entirely clear to me that this is needed, or that this is
+# the right way to do it. but I wanted to capture the way to handle typing
+# of something like this
+class Task(NamedTuple, Generic[TaskDetails]):
+    """Generic to contain taskid and the client-specific TaskDetails.
 
-    @classmethod
-    def from_db_row(row):
-        ...
+    """
+    taskid: str
+    task_details: TaskDetails
