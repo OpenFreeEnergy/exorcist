b7a1b69022ee8c2dae45fe5ec8ba582dc3465753
David W.H. Swenson <david.swenson@omsf.io>
2023-04-19 18:20:36 -0500

    Methods to create an empty TaskStatusDB

--- exorcist/taskdb.py
+++ exorcist/taskdb.py
@@ -1,40 +1,162 @@
 import sqlalchemy as sqla
 
+# remaining imports are for typing
+from typing import Optional, Iterable
+from .models import TaskStatus, Task
+from os import PathLike
+import networkx as nx
 
-def create_task_db(engine):
+
+class NoStatusChange(Exception):
+    """Raised when an attempt to change task status does not change status.
+
+    In most cases, like when a task status is changed to "completed", if the
+    task is already marked as the new status, this indicates database
+    corruption.
+
+    Raising this exception has two purposes:
+
+    1. To enable it to be caught and allow the database corruption to be
+       fixed, if possible, and exit otherwise.
+    2. So that, in the event that the status that needs to be set on early
+       exit (e.g., due to a sigterm send by the queuing system) is the same
+       as the current status, it can be caught without error and exit
+       normally.
     """
-    Create an empty task database with the provided engine.
+
+
+class TaskStatusDB:
+    """Database for managing execution and orchestration of tasks.
     """
-    ...
+    def __init__(self, engine: sqla.Engine):
+        metadata = sqla.MetaData()
+        metadata.reflect(engine)
+        if self._is_empty_db(metadata):
+            self._create_empty_db(metadata, engine)
+        elif not self._is_our_db(metadata):
+            raise RuntimeError(f"Database at {engine} does not seem "
+                               "to be a task database")
 
+        self.metadata = metadata
+        self.engine = engine
 
-def load_task_db(engine):
-    ...
+    def __repr__(self):
+        return f"TaskStatusDB(engine={self.engine})"
 
+    @property
+    def tasks_table(self):
+        return self.metadata.tables['tasks']
 
-
-def _ensure_uri(filename: str):
-    """If this is a filename, convert to a URI. If URI, leave as-is"""
-    ...
-
-
-
-class TaskDB:
-    def __init__(self, engine, mode):
-        ...
+    @property
+    def dependencies_table(self):
+        return self.metadata.tables['dependencies']
 
     @classmethod
-    def from_sqlite(cls, filename: str, mode: str):
-        uri = _ensure_uri(filename)
-        engine = ...
-        return cls(engine, mode=mode)
+    def from_filename(cls, filename: PathLike, *, overwrite: bool = False,
+                    **kwargs):
+        """Create an sqlite dialect database from a filename.
 
-    def _create_empty_db(self):
+        Parameters
+        ----------
+        filename : os.PathLike
+            Filename for the database
+        overwrite: bool
+            If True, all tables will be dropped from the existing database
+        kwargs :
+            Additional keyword arguments will be passed to
+            ``sqlalchemy.create_engine``. Particularly useful for debugging
+            is ``echo=True``, which will output the SQL statements being
+            generated internally.
+        """
+        engine = sqla.create_engine(f"sqlite:///{filename}", **kwargs)
+        if overwrite:
+            metadata = sqla.MetaData()
+            metadata.reflect(bind=engine)
+            metadata.drop_all(engine)
+
+        return cls(engine)
+
+    @staticmethod
+    def _is_empty_db(metadata: sqla.MetaData) -> bool:
+        return len(metadata.tables) == 0
+
+    @staticmethod
+    def _is_our_db(metadata: sqla.MetaData) -> bool:
+        # TODO: implement this: this should test whether the database we get
+        # has the schema we expect, and raise an error if not
+        return True
+
+    @staticmethod
+    def _create_empty_db(metadata, engine):
+        """Create the tables if our database is missing them"""
+        tasks_table = sqla.Table(
+            "tasks",
+            metadata,
+            sqla.Column("taskid", sqla.String, primary_key=True),
+            sqla.Column("status", sqla.Integer),
+            sqla.Column("last_modified", sqla.DateTime),
+            sqla.Column("tries", sqla.Integer),
+        )
+        deps_table = sqla.Table(
+            "dependencies",
+            metadata,
+            sqla.Column("from", sqla.String, sqla.ForeignKey("tasks.taskid")),
+            sqla.Column("to", sqla.String, sqla.ForeignKey("tasks.taskid")),
+        )
+        # TODO: create indices that may be needed
+        metadata.create_all(bind=engine)
+
+    def add_task(self, task: Task, requirements: Iterable[Task]):
+        """Add a task to the database.
+
+        Parameters
+        ----------
+        task: Task
+            the task to add to the database
+        requirements: Iterable[Task]
+            tasks that directly block the task to be added (typically, whose
+            outputs are inputs to the task)
+        """
         ...
 
-    def _load_existing_db(self):
+    def add_task_network(self, task_network: nx.DiGraph):
+        """Add a network of tasks to the database.
+
+        Parameters
+        ----------
+        task_network: nx.Digraph
+            A network with :class:`.Task`\ s as nodes.
+        """
         ...
 
-    def _load_tasks(self):
+    def update_task_status(
+        self,
+        taskid: str,
+        status: TaskStatus,
+        old_status: Optional[TaskStatus] = None
+    ):
+        """
+        Parameters
+        ----------
+        taskid: str
+            task ID of the task to update
+        status: TaskStatus
+            the status to change to
+        old_status: TaskStatus
+            the previous status
+
+        Raises
+        ------
+        NoStatusChange :
+            If the status does not actually change (e.g., the code is trying
+            to change it to a status it already has), this is raised. This
+            allows callers to catch and handle this case.
+        """
         ...
 
+    def mark_task_completed(self, completed_taskid: str):
+        """
+        Update the database (including the DAG info) to show that the task
+        has been completed.
+        """
+        ...
